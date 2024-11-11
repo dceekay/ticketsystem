6315fbcde925fcf64572cffdff7bde61aef35f31
Mohammad <muhddck@gmail.com>
2024-11-11 00:42:50 -0800

    Add ticket and payment routes and integrate with index.js

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const db = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const ticketRoutes = require('./routes/ticketRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
+
 app.use(express.json());
+
+app.use('/api/tickets', ticketRoutes);
+app.use('/api/payments', paymentRoutes);
 
 app.get('/', (req, res) => {
   res.send('Ticket and Payment Management System API');
