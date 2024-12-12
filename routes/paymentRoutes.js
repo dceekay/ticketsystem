00ec5e9475ef515166b714da4f83f2e0d854e6e9
Mohammad <muhddck@gmail.com>
2024-12-12 00:41:38 -0800

    Remove all existing files

--- routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentController = require('../controllers/paymentController');
-
-router.post('/', paymentController.createPayment);
-router.get('/', paymentController.getAllPayments);
-
-module.exports = router;
--- routes/ticketRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ticketController = require('../controllers/ticketController');
-
-router.post('/', ticketController.createTicket);
-router.get('/', ticketController.getAllTickets);
-
-module.exports = router;
